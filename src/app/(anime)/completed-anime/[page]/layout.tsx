6cabf682b18f17fd8c34223b8554e84799488973
Aortadev <anggahackerid@gmail.com>
2025-03-30 21:12:47 +0700

    Update layout.tsx

--- src/app/(anime)/completed-anime/[page]/layout.tsx
+++ src/app/(anime)/completed-anime/[page]/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next/types";
 
 export const metadata: Metadata = {
   title: "Completed Anime | Otakudesu",
-  description: "Completed Anime Page Otakudesu. Build by Rizky Haksono",
+  description: "Completed Anime Page Otakudesu. Build by Aortadev",
 };
 
 export default function CompletedAnimeLayout({
