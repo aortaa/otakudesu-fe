9c991b77056e3db497285b29cbb0a50003a7fd4a
Aortadev <anggahackerid@gmail.com>
2025-03-30 21:10:15 +0700

    Update layout.tsx

--- src/app/(movie)/movie/layout.tsx
+++ src/app/(movie)/movie/layout.tsx
@@ -3,7 +3,7 @@ import BaseLayout from "@/components/layout/base-layout";
 
 export const metadata: Metadata = {
   title: "Movies | Otakudesu",
-  description: "Movies Page Otakudesu. Build by Rizky Haksono",
+  description: "Movies Page Otakudesu. Build by Aortadev",
 };
 
 export default function MoviesLayout({ children }: Readonly<{ children: React.ReactNode }>) {
